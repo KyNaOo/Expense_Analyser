3571976ec98f50e260341f2a27f6e1f4e4aae9ed
KyNaOo <qinhao.wu122@gmail.com>
2024-09-23 16:23:33 +0200

    feat: result routing

--- src/app/form/page.tsx
+++ src/app/form/page.tsx
@@ -42,7 +42,7 @@ const SimpleForm: React.FC = () => {
   const handleSubmit = (e: FormEvent<HTMLFormElement>) => {
     e.preventDefault();
     localStorage.setItem("formData", JSON.stringify(formData));
-    window.location.href = "/result";
+    router.push("/result");
   };
 
   const handleCancel = () => {
